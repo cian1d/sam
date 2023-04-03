1e090f5592c6ceae46cc50b2b2654709d31c03c1
egorovgleb <sdfsd@ksjf.jkshd>
2023-04-03 20:16:42 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.myapplication.Frag1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyAdapter adapter = new MyAdapter(getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter); // устанавливаем адаптер
+        viewPager.setCurrentItem(1); // выводим второй экран
+    }
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+
+        MyAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new Frag1();
+                case 1:
+                    return new Frag2();
+
+                default:
+                    return new Frag1();
+            }
+        }
+    }
+}
